2abf7ca59abf159d0503e9c23421a28245873038
Matt Devey <matt.devey@endurance.com>
2017-12-20 13:25:01 -0700

    finished app

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const Promise = require('promise');
 const cmd = require('node-cmd');
 const axios = require('axios');
 
-var googleapi = 'https://maps.googleapis.com/maps/api/geocode/json?';
+// var googleapi = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 
-console.log(process.argv);
-// if(process.argv) {
-//     googleapi += process.argv[0];
-//     axios.get(googleapi)
-//     .then(function(res) {
-//         console.log(res);
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
-// }
-// else {
-//     console.log("enter a value after the command.");
-// }
+if(process.argv.length > 2) {
+    var googleapi = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + process.argv[2];
+    axios.get(googleapi)
+    .then(function(res) {
+        console.log(process.argv[2] + "\'s Data: ");
+        console.log("Latitude: ",res.data.results[0].geometry.location.lat);
+        console.log("Longitude: ", res.data.results[0].geometry.location.lng);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+else {
+    console.log("enter a value after the command.");
+}
